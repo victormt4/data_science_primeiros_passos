148b609d4d9866008961a941becf2b1476f2d586
Victor Mota <=victor.vmt@outlook.com>
2020-05-16 17:17:40 -0300

    Melhorando exemplos de gráficos de categoria

--- graficos_categoria.py
+++ graficos_categoria.py
@@ -6,11 +6,21 @@ movies = pd.read_csv('./tmdb_5000/tmdb_5000_movies.csv')
 
 print(movies.original_language.unique())  # valores únicos
 
-language_freq = movies['original_language'].value_counts().to_frame().reset_index()
-language_freq.columns = ['original_language', 'count']  # alterando nome das colunas
+language_count = movies['original_language'].value_counts().to_frame().reset_index()
+language_count.columns = ['original_language', 'total']  # alterando nome das colunas
 
-sns.barplot(data=language_freq, x='original_language', y='count')
+sns.barplot(data=language_count, x='original_language', y='total')
 plt.show()
 
-sns.catplot(data=movies, kind='count', x="original_language")
+# sns.catplot(data=movies, kind='count', x="original_language")
+# plt.show()
+
+total_en = movies['original_language'].value_counts().loc['en']
+total_other = movies['original_language'].value_counts().sum() - total_en
+
+data = pd.DataFrame({
+    'language': ['english', 'other'],
+    'total': [total_en, total_other]
+})
+sns.barplot(data=data, x='language', y='total')
 plt.show()
