b84a3609615f7195e9a69fa513f2ef1503a381ca
Victor Mota <=victor.vmt@outlook.com>
2020-05-16 09:26:28 -0300

    Adicionando exemplos de gráficos

--- introducao.py
+++ introducao.py
@@ -2,13 +2,32 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dataset = pd.read_csv('./ml-latest-small/ratings.csv')
+ratings = pd.read_csv('./ml-latest-small/ratings.csv')
 
-print('Media: ', dataset.rating.mean())
-print('Mediana: ', dataset.rating.median())
+print('Media: ', ratings.rating.mean())
+print('Mediana: ', ratings.rating.median())
 
-dataset.rating.plot(kind='hist')
+plt.title('Historiograma da frequencia das notas:')
+ratings.rating.plot(kind='hist')
 plt.show()
 
-sns.boxplot(dataset.rating)
+plt.title('Boxplot da frequencia das notas:')
+# plt.figure(figsize=(5, 5))
+sns.boxplot(ratings.rating)
+plt.show()
+
+movies = pd.read_csv('./ml-latest-small/movies.csv')
+
+ratings_per_movie = ratings.groupby('movieId').mean().rating
+
+plt.title('Historiograma da media das notas por filme:')
+ratings_per_movie.plot(kind='hist')
+plt.show()
+
+plt.title('Boxplot da media das notas por filme:')
+sns.boxplot(ratings_per_movie)
+plt.show()
+
+plt.title('Historiograma da media das notas por filme usando Seaborn:')
+sns.distplot(ratings_per_movie)
 plt.show()
