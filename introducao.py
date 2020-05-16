14998f853fff4a61fe5c62518e9b87d279d9348c
Victor Mota <=victor.vmt@outlook.com>
2020-05-16 08:51:18 -0300

    Criando nova plotagem com Seaborn

--- introducao.py
+++ introducao.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 dataset = pd.read_csv('./ml-latest-small/ratings.csv')
 
@@ -7,4 +8,7 @@ print('Media: ', dataset.rating.mean())
 print('Mediana: ', dataset.rating.median())
 
 dataset.rating.plot(kind='hist')
+plt.show()
+
+sns.boxplot(dataset.rating)
 plt.show()
